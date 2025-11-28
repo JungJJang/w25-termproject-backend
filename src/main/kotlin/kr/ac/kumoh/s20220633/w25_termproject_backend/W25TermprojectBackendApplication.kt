ca11d2d69a8c85102cb5f42cb930ee66bf92d5c2
JungJJang <ktkt28227027@gmail.com>
2025-11-28 13:26:07 +0900

    🎉Project 시작

---
+++ src/main/kotlin/kr/ac/kumoh/s20220633/w25_termproject_backend/W25TermprojectBackendApplication.kt
@@ -0,0 +1,11 @@
+package kr.ac.kumoh.s20220633.w25_termproject_backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class W25TermprojectBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<W25TermprojectBackendApplication>(*args)
+}
---
+++ src/test/kotlin/kr/ac/kumoh/s20220633/w25_termproject_backend/W25TermprojectBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.s20220633.w25_termproject_backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class W25TermprojectBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
